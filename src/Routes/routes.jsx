829c24e3d8ad7f672219cd841c3ba172fc2ed095
Deepan-B <bkavin04@gmail.com>
2024-04-20 19:55:03 +0530

    fixed the error

--- src/Routes/routes.jsx
+++ src/Routes/routes.jsx
@@ -1,28 +1,45 @@
-import { Route, Routes } from 'react-router-dom'
-import Home from '../pages/Home'
-import Doctors from '../pages/Doctors/Doctors'
-import DoctorDetails from '../pages/Doctors/DoctorDetails'
-import Login from '../pages/Login'
-import Signup from '../pages/Signup'
-import Contact from '../pages/Contact'
-import Services from '../pages/Services'
-import MyAccount from '../Dashboard/user-account/myAccount'
-import Dashboard from '../Dashboard/doctor-account/Dashboard'
-import ProtectedRoutes from './ProtectedRoutes'
+import { Route, Routes } from "react-router-dom";
+import Home from "../pages/Home";
+import Doctors from "../pages/Doctors/Doctors";
+import DoctorDetails from "../pages/Doctors/DoctorDetails";
+import Login from "../pages/Login";
+import Signup from "../pages/Signup";
+import Contact from "../pages/Contact";
+import Services from "../pages/Services";
+
+import Dashboard from "../Dashboard/doctor-account/Dashboard";
+import ProtectedRoutes from "./ProtectedRoutes";
+import MyAccount from "../Dashboard/user-account/MyAccount.jsx";
 
 const routes = () => {
-    return <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/home' element={<Home />} />
-        <Route path='/doctors' element={<Doctors />} />
-        <Route path='/doctors/:id' element={<DoctorDetails />} />
-        <Route path='/login' element={<Login/>} />
-        <Route path='/register' element={<Signup />} />
-        <Route path='/contact' element={<Contact />} />
-        <Route path='/services' element={<Services />} />
-        <Route path='/users/profile/me' element={<ProtectedRoutes allowedRoles={['patient']}><MyAccount /></ProtectedRoutes>} />
-        <Route path='/doctors/profile/me' element={<ProtectedRoutes allowedRoles={['doctor']}><Dashboard /></ProtectedRoutes>} />
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/doctors" element={<Doctors />} />
+      <Route path="/doctors/:id" element={<DoctorDetails />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Signup />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/services" element={<Services />} />
+      <Route
+        path="/users/profile/me"
+        element={
+          <ProtectedRoutes allowedRoles={["patient"]}>
+            <MyAccount />
+          </ProtectedRoutes>
+        }
+      />
+      <Route
+        path="/doctors/profile/me"
+        element={
+          <ProtectedRoutes allowedRoles={["doctor"]}>
+            <Dashboard />
+          </ProtectedRoutes>
+        }
+      />
     </Routes>
-}
+  );
+};
 
-export default routes
+export default routes;
