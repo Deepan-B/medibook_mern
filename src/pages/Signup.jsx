1e6a581ac6c9430dbd1b7e78900673fa6698c991
Deepan-B <bkavin04@gmail.com>
2024-04-20 23:36:55 +0530

    changed the ui of profile-2

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -165,7 +165,7 @@ const Signup = () => {
                     <img
                       src={PreviewURL}
                       alt=""
-                      className=" max:w-[50px] max:h-[50px] object-contain rounded-full"
+                      className=" max-w-[50px] max-h-[50px] object-contain rounded-full"
                     />
                   </figure>
                 )}
